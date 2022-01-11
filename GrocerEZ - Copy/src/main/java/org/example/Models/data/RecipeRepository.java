55fa1128d223dc46310b6051bff41b734667a404
krisrehbein <84356496+krisrehbein@users.noreply.github.com>
2022-01-11 16:29:22 -0600

    Delete GrocerEZ - Copy directory
    cleaning the repo

--- GrocerEZ - Copy/src/main/java/org/example/Models/data/RecipeRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.Models.data;
-
-import org.example.Models.Recipe;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RecipeRepository extends CrudRepository<Recipe, Integer> {
-}
